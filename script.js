8de192cabfe18d3b2449f875d217d8f2f9b54b45
Tensen <thienmpham@gmail.com>
2025-10-28 19:44:21 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary() {}
