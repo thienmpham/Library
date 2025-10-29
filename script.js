df6df94e0b25812f238de1b364ae66cc8da608a9
Tensen <thienmpham@gmail.com>
2025-10-29 18:50:22 -0400

    Logic for adding books to myLibrary array

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const myLibrary = [];
 
-function Book(title, author, pages, status) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.status = status;
+  //   this.status = status;
 }
 
-function addBookToLibrary() {}
+function addBookToLibrary(book) {
+  if (typeof book !== "object") {
+    console.log("book is not an object");
+    return;
+  }
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
+
+let book1 = new Book("Berserk", "Kentaro Miura", "362");
+addBookToLibrary(book1);
