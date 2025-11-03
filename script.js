890182107f4ef5df29d87ab6c33dca4563723b86
Tensen <thienmpham@gmail.com>
2025-11-02 21:14:02 -0500

    Adding the form inputs

--- script.js
+++ script.js
@@ -16,5 +16,19 @@ function addBookToLibrary(book) {
   console.log(myLibrary);
 }
 
-let book1 = new Book("Berserk", "Kentaro Miura", "362");
-addBookToLibrary(book1);
+function originalBooks() {
+  let book1 = new Book("Berserk", "Kentaro Miura", "362");
+  let book2 = new Book("Maze Runner", "James Dashner", "416");
+  let book3 = new Book("Renegades", "Marissa Meyer", "552");
+  addBookToLibrary(book1);
+  addBookToLibrary(book2);
+  addBookToLibrary(book3);
+}
+originalBooks();
+
+function iterateMyLibrary() {
+  for (book of myLibrary) {
+    console.log(book.title);
+  }
+}
+iterateMyLibrary();
